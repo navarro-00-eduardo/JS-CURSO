143e3f7e8eef6a99b3ae5a7c6d69ac1cc8c87969
Eduardo Navarro <navarro.eduardo123456@gmail.com>
2021-04-15 17:12:01 -0500

    clase 1 variables

---
+++ variables.js
@@ -0,0 +1,79 @@
+//Tipos de datos
+//Boleano= true o false
+var Boleano = true
+//string (conjunto de caracteres)
+var Eduardo = "Flaco"
+//undefined= undefined
+var indefinido = undefined
+// Null variable sin valor
+var Nulo = null
+//Number (especifica un numero, enteros y flotantes=(decimales)
+var Numero = 27
+var Decimal = 10.20
+//objeto (Los objetos contienen una estructura de datos que mantienen un contexto)
+var objetos={
+    nombre: 'Lalo',
+    años: 20,
+    location: undefined,
+    average:12.12,
+    hobbies:['hiking', 'run ', 'travel']
+
+}
+//Arreglos (son similares a los objetos pero, sus valores van apuntados a un indice)
+var miarreglo = [1, "Lalo", true, 20, null,undefined ]
+
+
+// ejercicio 1: Crear  diferentes variables usando cada tipo de dato
+//3 string
+var Nombre = "Eduardo"
+var Apellido = "Navarro"
+var Apodo = "Wuero"
+
+// 3 tipo number
+
+var edad = 20
+var estatura = 2
+var gastos = 2000.75
+
+// 3 bolean
+var mentira = false
+var veridico = true
+var donadorOrganos = true
+
+// 3 undefined
+var indefinido = undefined
+var ahorros = undefined
+var ganancia = undefined
+
+//  3 Null 
+var estudios = null
+var compra = null
+var reporte = null
+
+// 3 objetos
+var actividades = {
+    limpieza:"Toda la casa",
+    orden:true,
+    actExtra: 1
+}
+var calificaciones = {
+    materia:" Fundamentos de microcontroladores",
+    promedio: 98,
+    acreditada: false
+}
+var libros = {
+    comprados:3,
+    clase:"drama",
+    otrolenguaje: false
+}
+//3 arreglos
+var miarreglo = [1, "Lalo", true, 20, null,undefined ]
+var infoPersonal = [2, "eduardo", false, 20, undefined ]
+var historialVentas = [8, "electronicos", true, 88, undefined ]
+
+
+
+console.log("Holamundo")
+
+//ejercicio 3 imprimir el tipo de dato de nuestras variables
+console. log ("Tipo de dato=", typeof miarreglo)
